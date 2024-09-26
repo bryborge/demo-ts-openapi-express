099bbc1da4d304cd837a9b30b4da7658df44db12
Bryan Borgeson <bryborge@gmail.com>
2024-09-26 15:03:09 -0700

    swap out mongo adapter for mongoose

--- src/config/init.ts
+++ src/config/init.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express';
-import { MongoAdapter } from '../db/mongoAdapter';
-import { env } from 'process';
+import { env, exit } from 'process';
+// TODO: Encapsulate in it's own module
+import mongoose from 'mongoose';
 
 /**
  * Initializes the Express app.
@@ -10,21 +11,34 @@ import { env } from 'process';
  * @returns {Promise<void>} A promise that resolves when the app is listening.
  */
 const appInit = async (app: Express): Promise<void> => {
-  const mongoUri = env.MONGO_URI || 'mongodb://localhost:27017';
-  const mongoDbName = env.MONGO_DB_NAME || 'todoApp';
-
-  const dbAdapter = new MongoAdapter(mongoUri, mongoDbName);
-
   try {
     const PORT = Number(process.env.APP_PORT) || 8080;
 
     app.listen(PORT, async () => {
-      await dbAdapter.connect();
+      await dbConnect();
       console.log(`Server started on port ${PORT}`);
     });
   } catch(error: unknown) {
     console.log('Unable to start the app!');
     console.error(error);
+  }
+};
+
+// TODO: Encapsulate in it's own module
+const dbConnect = async (): Promise<void> => {
+  const mongoDbName = env.MONGO_DB_NAME as string;
+  const mongoUsername = env.MONGO_USERNAME as string;
+  const mongoPassword = env.MONGO_PASSWORD as string;
+  const mongoHost = env.MONGO_HOST as string;
+  const mongoPort = Number(env.MONGO_PORT) || 27017;
+  const mongoUri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDbName}?authSource=admin`;
+
+  try {
+    await mongoose.connect(mongoUri);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    exit(1);
   }
 };
 
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,12 @@
-import * as v from 'valibot';
-import { vValidator } from 'validation-adapters/valibot';
+// import * as v from 'valibot';
+// import { vValidator } from 'validation-adapters/valibot';
+// import { operations } from '../generated/v1';
 
+// Type Definitions
+import { Request, Response } from 'express';
+// Models
+import UserModel from '../models/UserModel';
+// Base Controller
 import BaseController from './base.controller';
 
 class UsersController extends BaseController {
@@ -10,25 +16,42 @@ class UsersController extends BaseController {
   }
 
   protected initializeRoutes(): void {
-    this.openApiRouter.get('/users/{userId}', {
-      pathValidator: vValidator(
-        v.object({
-          userId: v.number(),
-        })  
-      ),
-      handler: this.getUserById,
+    this.openApiRouter.get('/users', {
+      handler: this.getUsers,
     });
+
+    // this.openApiRouter.get('/users/{userId}', {
+    //   pathValidator: vValidator(
+    //     v.object({
+    //       userId: v.number(),
+    //     })  
+    //   ),
+    //   handler: this.getUserById,
+    // });
   }
 
-  private getUserById(req: any, res: any) {
-    const { userId } = req.params;
-
-    res.json({
-      id: userId,
-      username: 'user1',
-      email: 'user1@example.com',
-    });
+  private async getUsers(_req: Request, res: Response) {
+    try {
+      const users = await UserModel.find({});
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching users', error });
+    }
   }
+
+  // private async getUserById(req: Request, res: Response) {
+  //   try {
+  //     const userId = req.params.userId;
+  //     const user = await UserModel.findById(userId);
+
+  //     if (!user) {
+  //       return res.status(404).json({ message: 'User not found' });
+  //     }
+  //     res.status(200).json(user);
+  //   } catch (error) {
+  //     res.status(500).json({ message: 'Error fetching user', error });
+  //   }
+  // }
 }
 
 export default UsersController;
