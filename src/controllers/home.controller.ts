1ba5bc3b83519374fe56af7cef2e56549611771e
Bryan Borgeson <bryborge@gmail.com>
2024-09-26 17:17:23 -0700

    Complete CRUD ops on users

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -6,13 +6,24 @@ class HomeController extends BaseController {
     this.initializeRoutes();
   }
 
+  /**
+   * Initialize the routes for the Home controller.
+   */
   protected initializeRoutes(): void {
     this.openApiRouter.get('/', {
       handler: this.getStatus,
     });
   }
 
-  private getStatus = (_req: any, res: any) => {
+  /**
+   * Retrieves the current status of the API.
+   *
+   * @param _req The Express request object.
+   * @param res The Express response object.
+   *
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
+  private getStatus(_req: any, res: any): void {
     res.json({
       status: 'operational',
       uptime: Number(process.uptime()),
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -15,6 +15,17 @@ class UsersController extends BaseController {
     this.initializeRoutes();
   }
 
+  /**
+   * Initialize the routes for the Users controller.
+   *
+   * This method sets up and type-validates the following routes:
+   *
+   * - `GET /users`: Returns a list of all users.
+   * - `GET /users/{username}`: Returns the user with the given username.
+   * - `POST /users`: Creates a new user.
+   * - `PUT /users/{username}`: Updates the user with the given username.
+   * - `DELETE /users/{username}`: Deletes the user with the given username.
+   */
   protected initializeRoutes(): void {
     this.openApiRouter.get('/users', {
       handler: this.getUsers,
@@ -29,6 +40,10 @@ class UsersController extends BaseController {
       handler: this.getUserByUsername,
     });
 
+    this.openApiRouter.post('/users', {
+      handler: this.createUser,
+    });
+
     this.openApiRouter.put('/users/{username}', {
       pathValidator: vValidator(
         v.object({
@@ -37,8 +52,25 @@ class UsersController extends BaseController {
       ),
       handler: this.updateUser,
     });
+
+    this.openApiRouter.del('/users/{username}', {
+      pathValidator: vValidator(
+        v.object({
+          username: v.string(),
+        })  
+      ),
+      handler: this.deleteUser,
+    });
   }
 
+  /**
+   * Retrieves a list of all users.
+   * 
+   * @param _req The Express request object.
+   * @param res The Express response object.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
   private async getUsers(_req: Request, res: Response) {
     try {
       const users = await UserModel.find({});
@@ -48,6 +80,14 @@ class UsersController extends BaseController {
     }
   }
 
+  /**
+   * Retrieves a user by username.
+   * 
+   * @param req The Express request object.
+   * @param res The Express response object.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
   private async getUserByUsername(req: Request, res: Response) {
     try {
       const username = req.params.username;
@@ -62,6 +102,33 @@ class UsersController extends BaseController {
     }
   }
 
+  /**
+   * Creates a new user.
+   *
+   * @param req The Express request object.
+   * @param res The Express response object.
+   *
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
+  public async createUser(req: Request, res: Response) {
+    try {
+      const userData: operations['createUser']['requestBody']['content']['application/json'] = req.body;
+      const user = await UserModel.create(userData);
+      res.status(201).json(user);
+    } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(400).json({ message: 'Error creating user', error: error instanceof Error ? error.message : String(error) });
+    }
+  }
+
+  /**
+   * Updates a user by username.
+   * 
+   * @param req The Express request object.
+   * @param res The Express response object.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
   private async updateUser(req: Request, res: Response) {
     try {
       const username = req.params.username;
@@ -76,6 +143,29 @@ class UsersController extends BaseController {
     } catch (error) {
       console.error('Error updating user:', error);
       res.status(400).json({ message: 'Error updating user', error: error instanceof Error ? error.message : String(error) });
+    }
+  }
+
+  /**
+   * Deletes a user by username.
+   * 
+   * @param req The Express request object.
+   * @param res The Express response object.
+   * 
+   * @returns {Promise<void>} A promise that resolves when the operation is complete.
+   */
+  private async deleteUser(req: Request, res: Response) {
+    try {
+      const username = req.params.username;
+      const deletedUser = await UserModel.findOneAndDelete({username});
+      
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.status(204).send();
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      res.status(500).json({ message: 'Error deleting user', error: error instanceof Error ? error.message : String(error) });
     }
   }
 }
